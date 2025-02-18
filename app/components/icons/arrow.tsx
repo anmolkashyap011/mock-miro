54ed5eb1bd4f61bd27018292a0e943a1ca9a749b
Anmol <anmolkashyap8060@gmail.com>
2025-02-18 13:41:32 +0530

    Add Arrow icon component: Create a reusable SVG icon with customizable fill

---
+++ app/components/icons/arrow.tsx
@@ -0,0 +1,12 @@
+interface IconProps extends React.SVGAttributes<SVGElement> {
+
+}
+
+export default function Arrow(props: IconProps) {
+    return (
+        <svg width="31" height="30" viewBox="0 0 31 30" fill={props.fill} xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path d="M16.6776 21L15.6932 20.0284L19.7457 15.9759H9.25V14.5696H19.7457L15.6932 10.5298L16.6776 9.54545L22.4048 15.2727L16.6776 21Z" fill={props.fill}/>
+        </svg>
+
+    )
+}
