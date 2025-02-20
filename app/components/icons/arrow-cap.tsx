0dad228ecffdb461bed19d42205e8b6233d0004a
Anmol <anmolkashyap8060@gmail.com>
2025-02-20 13:07:18 +0530

    Add ArrowCap component: Create SVG icon for arrow representation

---
+++ app/components/icons/arrow-cap.tsx
@@ -0,0 +1,16 @@
+import { SVGAttributes } from "react";
+
+export default function ArrowCap(props: SVGAttributes<SVGElement>) {
+    return (
+        <svg width="31" height="30" viewBox="0 0 31 30" stroke="inherit" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <g clipPath="url(#clip0_4_119)">
+        <path d="M10.6562 12L15.6562 17L20.6562 12" stroke="inherit" strokeWidth="1.5"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_4_119">
+        <rect width="12" height="8" fill="white" transform="translate(9.65625 11)"/>
+        </clipPath>
+        </defs>
+        </svg>
+    )
+}
